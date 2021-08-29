b32eecd816c1f45d70a095f70800e5cb454c9d9a
eduardopereiraboares <eduardop.boares@gmail.com>
2021-08-29 17:02:08 -0300

    Feature of forgot and reset password implemented

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import '@modules/users/providers';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import { container } from 'tsyringe';
 import './providers';
 
@@ -14,4 +16,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUserRepository>(
     'UsersRepository',
     UsersRepository
+);
+
+container.registerSingleton<IUserTokensRepository>(
+    'UserTokensRepository',
+    UserTokensRepository
 );
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,5 @@
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import { Router } from 'express';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
