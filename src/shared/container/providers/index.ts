7bbec3b4332aa330721e2ef3ac239a6fc153bcde
eduardopereiraboares <eduardop.boares@gmail.com>
2021-11-15 18:26:19 -0300

    [FEATURE] Email template provider implemented

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,6 +1,8 @@
 import { container } from 'tsyringe';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
@@ -9,7 +11,12 @@ container.registerSingleton<IStorageProvider>(
     DiskStorageProvider
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+    'MailTemplateProvider',
+    HandlebarsMailTemplateProvider
+);
+
 container.registerInstance<IMailProvider>(
     'MailProvider',
-    new EtherealMailProvider()
+    container.resolve(EtherealMailProvider)
 );
